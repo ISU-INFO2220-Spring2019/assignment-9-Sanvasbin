635372354cff2fa957b566650384bd840c11ed11
Sabin Shrestha <43sanrizz@gmail.com>
2019-04-17 23:07:16 -0600

    Finished

--- scripts/viewPages.js
+++ scripts/viewPages.js
@@ -79,8 +79,8 @@ function displayPeople() {
     var data = JSON.parse(json);
     for (var i = 0; i < data.length; i++) {
         var li = $("<li></li>").addClass('best')
-            .text((data[i].name) + " " + (data[i].phone) + " " + (data[i].age) + " " + (data[i].group));
-        var btnEdit = $("<button> Edit</button>");
+            .text("Name: " + (data[i].name) + " " + "Phone: " + (data[i].phone) + " " + "Age: " + (data[i].age) + " " + "Group: " + (data[i].group));
+        var btnEdit = $("</br><button>Edit</button>");
         li.append(btnEdit);
         list.append(li);
         if (data[i].group == 1) {
